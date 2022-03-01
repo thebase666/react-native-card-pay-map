85c241a2f728031736f9760a1c93ec7d7471f6c9
thebase666 <lizhuyao666@gmail.com>
2022-03-01 15:32:09 +0700

    day

--- App.js
+++ App.js
@@ -1,12 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet } from 'react-native';
+import FoodDetail from './screens/FoodDetail';
+import MyCart from './screens/MyCart';
+import MyCard from './screens/MyCard';
+import AddCard from './screens/AddCard';
+import Checkout from './screens/Checkout';
+import Success from './screens/Success';
+import DeliveryStatus from './screens/DeliveryStatus';
+import Map from './screens/Map';
+
+// import { createStore, applyMiddleware } from "redux";
+// import { Provider } from "react-redux";
+// import thunk from "redux-thunk";
+// import rootReducer from './store';
+// import Tab from './navigation/tabs'
+
+const Stack = createNativeStackNavigator();
+
+// const store = createStore(
+//   rootReducer,
+//   applyMiddleware(thunk)
+// )
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+        initialRouteName={"FoodDetail"}
+      >
+        <Stack.Screen name="FoodDetail" component={FoodDetail} />
+        <Stack.Screen name="MyCart" component={MyCart} />
+        <Stack.Screen name="MyCard" component={MyCard} />
+        <Stack.Screen name="AddCard" component={AddCard} />
+        <Stack.Screen name="Checkout" component={Checkout} />
+        <Stack.Screen name="Success" component={Success} />
+        <Stack.Screen name="DeliveryStatus" component={DeliveryStatus} />
+        <Stack.Screen name="Map" component={Map} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ components/CartQuantityButton.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { View, TouchableOpacity, Image, Text } from "react-native";
+
+import { COLORS, FONTS, SIZES, icons } from "../constants";
+
+const CartQuantityButton = ({
+  containerStyle,
+  iconStyle,
+  quantity,
+  onPress,
+}) => {
+  return (
+    <TouchableOpacity
+      style={{
+        width: 40,
+        height: 40,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: SIZES.radius,
+        backgroundColor: COLORS.lightOrange2,
+        ...containerStyle,
+      }}
+      onPress={onPress}
+    >
+      <Image
+        source={icons.cart}
+        style={{
+          width: 20,
+          height: 20,
+          tintColor: COLORS.black,
+          ...iconStyle,
+        }}
+      />
+
+      <View
+        style={{
+          position: "absolute",
+          top: 5,
+          right: 5,
+          height: 15,
+          width: 15,
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: SIZES.radius,
+          backgroundColor: COLORS.primary,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 8,
+          }}
+        >
+          {quantity}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default CartQuantityButton;
